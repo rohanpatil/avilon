197d31ebbc1ad5ce837f65f2b29019e335f81e33
Rohan Patil <rohan.patil@anyonehome.com>
2019-03-28 15:42:54 +0530

    Update sniff.php

--- sniff.php
+++ sniff.php
@@ -79,7 +79,7 @@ foreach ($arrURLs as $index => $strURL) {
 			break;
 		}
 		$channelname = substr(strrchr($value, ","), 1);
-		if (preg_match('^(?:hindi:|english:|marathi:|in:|in-|in -|\(in\)|in\||in \||hindi \||hindi\||hindi -|hindi-|english -|english-|english\||english \||marathi\||marathi \||adt|xxx)', strtolower(trim($channelname))) === 1) {
+		if (preg_match('/^(?:hindi:|english:|marathi:|in:|in-|in -|\(in\)|in\||in \||hindi \||hindi\||hindi -|hindi-|english -|english-|english\||english \||marathi\||marathi \||adt|xxx)/', strtolower(trim($channelname))) === 1) {
 			$strgroupTitle = $index;
 			$intTotalChannelCount++;
 
